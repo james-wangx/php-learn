48fc2b27cdfd88213cb02236e58ff05f2bf78997
Pineapple <pineapple_cpp@163.com>
2022-08-27 19:13:39 +0800

    PHP 基础语法

---
+++ php/hybrid.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  echo "如果 x 和 y 具有相同的键/值对，且顺序类型相同，则返回 true";
+  echo "<div style='height: 200px; width: 300px; border: solid 2px red'>" . `time /T` . "</div>";
+  ?>
+</body>
+</html>
